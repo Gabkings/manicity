5b65280b524a3e504138cc025e331af81b5a1f4b
dev <dev@localhost.localdomain>
2020-10-18 00:31:38 +0300

    added aniamtion for featured section

---
+++ src/components/home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Featured from './featured'
+
+function Home() {
+    return (
+        <div className="bck_blue">
+            <Featured />
+        </div>
+    )
+}
+
+export default Home
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Resources/css/app.css'
 import Layout from './Hoc/Layout'
+import {Switch, Route} from 'react-router-dom'
+import Home from './components/home/index'
 
 const Routes = (props) =>{
     return (
       <Layout>
-        second line
+        <Switch>
+          <Route component={Home} path="/" />
+        </Switch>
       </Layout>
     );
 }
