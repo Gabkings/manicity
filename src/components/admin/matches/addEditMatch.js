ea94e25b4f750ac6bfbcaa0abf09556e643d5c5f
dev <dev@localhost.localdomain>
2020-11-28 11:59:33 +0300

    Adding edit/add match component

---
+++ src/components/admin/matches/addEditMatch.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AddEditMatch extends Component {
+    render() {
+        return (
+            <div>
+                Add Edit Match
+            </div>
+        )
+    }
+}
